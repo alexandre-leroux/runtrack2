ca513989cada86ecff03022c7d5f405792f3e73d
prénom nom <alexandre.leroux@laplateforme.io>
2020-11-09 23:07:50 +0100

    job 3 ok

--- jour07/job03/index.php
+++ jour07/job03/index.php
@@ -36,37 +36,27 @@ session_start();
 
 <?php
 
+if (isset($_POST["reset"]))  {  session_destroy(); exit; }
 
 
-// $_SESSION[] = 'test';
-// $_SESSION[] = 'test 1';
-// $_SESSION[] = 'test 2';
 
-if (isset($_POST["prenom"])){    $_SESSION["prenom"][] = $_POST["prenom"];
- }
- if (isset($_POST["reset"])){         session_destroy();
- }
- if (isset($_SESSION["prenom"][0])){
-         var_dump($_SESSION);
+
+if (isset($_POST["prenom"]))     { $_SESSION["prenom"][] = $_POST["prenom"];
  }
 
-
-
-// $i=0;
-// while ( $_SESSION[$i]==true) {$i++;}
-// if ( $_POST['prénom'] == true) {  $_SESSION[$i] =  $_POST['prénom'] ;}
+ @$prenoms = $_SESSION["prenom"];
 
 
 
-// if ( @$_POST['prénom'] == true ) {$prenom = $_POST['prénom'] ; }
 
-// elseif ( @$_POST['reset'] == true ) { session_destroy() ; exit; }
-// for ( $i=0 ; isset($_SESSION[$i]) ; $i++ ) {
 
-//     $_SESSION[] = $_POST['prénom']
+ if (isset($_POST["prenom"])) {
 
-// }
-// echo $_SESSION;
+ foreach ( $prenoms as $key => $value){
+    echo $value. ' <br>';
+}
+ }
+
 
 
 
@@ -77,7 +67,7 @@ if (isset($_POST["prenom"])){    $_SESSION["prenom"][] = $_POST["prenom"];
 
 <pre>
 <?php
-var_dump($_SESSION) ;
+// var_dump($_SESSION) ;
 ?>
 </pre>
 
